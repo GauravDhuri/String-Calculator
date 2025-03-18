bf4101fb381851a9d7590dfadd754ba297a2fed3
Gaurav Dhuri <dhuriofficial@gmail.com>
2025-03-18 14:14:47 +0530

    Code refactor for better readability

--- string-calculator.js
+++ string-calculator.js
@@ -1,44 +1,68 @@
-function add(stringifiedNums) {
+function calculate(stringifiedNums) {
   let numbers;
+  let delimiters = [];
 
+  // Parse numbers and delimiters
   if (stringifiedNums.startsWith('//')) {
-    const delimiterEndIndex = stringifiedNums.indexOf('\n');
-    const delimiterList = stringifiedNums.slice(2, delimiterEndIndex);
-    const string = stringifiedNums.slice(delimiterEndIndex + 1); 
-
-    let delimiters = [];
-
-    if (delimiterList.startsWith('[')) {
-      delimiters = delimiterList.match(/\[([^\]]+)\]/g).map(d => d.slice(1, -1));
-    } else {
-      delimiters = [delimiterList];
-    }
-
-    const regex = new RegExp(`[${delimiters.join('|')}|\\n]`, 'g');
-
-    numbers = parseNumbers(string, regex);
+    const parsedData = customDelimeter(stringifiedNums, delimiters)
+    numbers = parsedData.parseNumbers;
   } else {
     numbers = parseNumbers(stringifiedNums);
   }
 
-  numbers = numbers.filter(num => num <= 1000);
-
-  checkForNegativeNumbers(numbers);
+  // Validate Numbers
+  numbers = validateNumbers(numbers);
   
-  return numbers.reduce((sum, num) => sum + num, 0);
-}
+  // Perform Operation
+  const result = performOperation(numbers, delimiters);
 
-function checkForNegativeNumbers(numbers) {
-  const negativeNums = numbers.filter(num => num < 0);
-  if (negativeNums.length > 0) {
-    throw new Error(`Negative numbers not allowed: ${negativeNums.join(', ')}`);
-  }
+  return result;
 }
 
 function parseNumbers(stringifiedNums, delimiter = /[\n,]/) {
   return stringifiedNums.split(delimiter).map(Number);
 }
 
+function customDelimeter(stringifiedNums, delimiters) {
+  const delimiterEndIndex = stringifiedNums.indexOf('\n');
+  const delimiterList = stringifiedNums.slice(2, delimiterEndIndex);
+  const string = stringifiedNums.slice(delimiterEndIndex + 1); 
+
+  if (delimiterList.startsWith('[')) {
+    delimiters = delimiterList.match(/\[([^\]]+)\]/g).map(d => d.slice(1, -1));
+  } else {
+    delimiters = [delimiterList];
+  }
+
+  const regex = new RegExp(`[${delimiters.join('|')}|\\n]`, 'g');
+
+  numbers = parseNumbers(string, regex);
+  return {
+    parseNumbers: numbers,
+  }
+}
+
+function validateNumbers(numbers) {
+  const negativeNums = numbers.filter(num => num < 0);
+  if (negativeNums.length > 0) {
+    throw new Error(`Negative numbers not allowed: ${negativeNums.join(', ')}`);
+  }
+
+  return numbers.filter(num => num <= 1000);
+}
+
+function performOperation(numbers, delimiters) {
+  let operator = '+';
+  if(delimiters.includes('*')) operator = '*';
+
+  switch(operator) {
+    case '+':
+      return numbers.reduce((sum, num) => sum + num, 0);
+    case '*':
+    return numbers.reduce((sum, num) => sum * num, 0);
+  }
+}
+
 module.exports = {
-  add
+  calculate
 }
--- test/string-calculator.test.js
+++ test/string-calculator.test.js
@@ -1,16 +1,16 @@
-const { add } = require("../String-Calculator");
+const { calculate } = require("../String-Calculator");
 
 describe('String calculator', () => {
   it('should return 0 for an empty string', () => {
-    expect(add('')).toBe(0);
+    expect(calculate('')).toBe(0);
   });
 
   it('should return number itself for a single number', () => {
-    expect(add('1')).toBe(1);
+    expect(calculate('1')).toBe(1);
   })
 
   it('return sum of two numbers', () => {
-    expect(add('1,2')).toBe(3);
+    expect(calculate('1,2')).toBe(3);
   })
 
   it('should be able return sum of n numbers (n is set to a limit of 100 for demo purpose)', () => {
@@ -18,44 +18,44 @@ describe('String calculator', () => {
     const input = numbers.join(',');
     const expected = numbers.reduce((sum, num) => sum + num, 0);
 
-    expect(add(input)).toBe(expected);
+    expect(calculate(input)).toBe(expected);
   })
 
   it('should be able to return sum of numbers separated by commas and new lines', () => {
-    expect(add('1,2\n3,4\n5')).toBe(15);
+    expect(calculate('1,2\n3,4\n5')).toBe(15);
   })
 
   it('should be able to return sum of numbers separated by custom delimiter ; and new lines', () => {
-    expect(add('//;\n1;2\n3;4;5')).toBe(15);
+    expect(calculate('//;\n1;2\n3;4;5')).toBe(15);
   })
 
   it('should be able to return sum of numbers separated by custom delimiter : and new lines', () => {
-    expect(add('//:\n1:2\n3:4\n5')).toBe(15);
+    expect(calculate('//:\n1:2\n3:4\n5')).toBe(15);
   })
 
   it('should be able to return sum of numbers separated by custom delimiter | and new lines', () => {
-    expect(add('//|\n1|2\n3|4|5')).toBe(15);
+    expect(calculate('//|\n1|2\n3|4|5')).toBe(15);
   })
 
   it('should throw an expection if list of numbers has negative value', () => {
-    expect(() => add('1,-2,3')).toThrow('Negative numbers not allowed: -2');
-    expect(() => add('//;\n1;-2;3')).toThrow('Negative numbers not allowed: -2');
-    expect(() => add('//:\n1:-2:3')).toThrow('Negative numbers not allowed: -2');
-    expect(() => add('//|\n1|-2|3')).toThrow('Negative numbers not allowed: -2');
+    expect(() => calculate('1,-2,3')).toThrow('Negative numbers not allowed: -2');
+    expect(() => calculate('//;\n1;-2;3')).toThrow('Negative numbers not allowed: -2');
+    expect(() => calculate('//:\n1:-2:3')).toThrow('Negative numbers not allowed: -2');
+    expect(() => calculate('//|\n1|-2|3')).toThrow('Negative numbers not allowed: -2');
   })
 
   it('should ignore numbers greater than 1000', () => {
-    expect(add('2,1001')).toBe(2);
-    expect(add('1000,1001,999')).toBe(1999);
-    expect(add('//;\n2;1001;999')).toBe(1001);
+    expect(calculate('2,1001')).toBe(2);
+    expect(calculate('1000,1001,999')).toBe(1999);
+    expect(calculate('//;\n2;1001;999')).toBe(1001);
   });
 
   it('should support multi-character delimiters', () => {
-    expect(add('//[***]\n1***2***3')).toBe(6);
+    expect(calculate('//[***]\n1***2***3')).toBe(6);
   });
 
   it('should be able to return sum of numbers separated by multiple custom delimiters', () => {
-    expect(add('//[*][%]\n1*2%3')).toBe(6);
-    expect(add('//[|][;]\n1|2;3\n4;10001|5')).toBe(15);
+    expect(calculate('//[*][%]\n1*2%3')).toBe(6);
+    expect(calculate('//[|][;]\n1|2;3\n4;10001|5')).toBe(15);
   });
 })
